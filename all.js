3e174e8058b9be7286bc6f1b72e163c89e353fc0
kyriehavegoodday <healthymbear@gmail.com>
2022-04-18 01:58:44 +0800

    更新後台及前台版型

--- all.js
+++ all.js
@@ -13,13 +13,13 @@ firebase.analytics();
 const db = firebase.database();
 
 // db.ref('banner/img5').set({link:'https://upload.cc/i1/2022/04/04/R0HPo5.jpg'})
-const banner =  db.ref('banner');
-banner.once('value',function(snapshot){
+const banner = db.ref('banner');
+banner.once('value', function (snapshot) {
     let todoNum = snapshot.val();
-    console.log(todoNum.img1.img);
+    // console.log(todoNum.img1.img);
     let bannerMain = document.getElementById('banner');
     let str = '';
-    for(item in todoNum){
+    for (item in todoNum) {
         str = `
         
         <img src="${todoNum[item].link}">
@@ -27,19 +27,19 @@ banner.once('value',function(snapshot){
         `
         bannerMain.innerHTML += str;
     }
-   
+
 })
 
 
 // db.ref('about/data3').set({text:'我是一位網頁設計師，專門客製化設計'})
 const aboutData = db.ref('about');
-aboutData.once('value',function(snapshot){
+aboutData.once('value', function (snapshot) {
     let aboutMe = document.getElementById('about-me');
     let str = '';
     let data = snapshot.val();
-   
-        // console.log(data);
-        str = `
+
+    // console.log(data);
+    str = `
         <h1 class="title" data-aos="fade-right" data-aos-duration="1000" data-aos-easing="ease-out-sine"
             data-aos-mirror="false">${data.data1.text}</h1>
         <h2 data-aos="fade-down" data-aos-offset="100" data-aos-duration="500" data-aos-delay="500"
@@ -47,9 +47,127 @@ aboutData.once('value',function(snapshot){
         <p>${data.data3.text}</p>
         
         `
-        
-        aboutMe.innerHTML = str;
 
-        
+    aboutMe.innerHTML = str;
+
+
+
+})
+
+// 第一組照片
+const introduce_img_one = db.ref('introduce_one');
+introduce_img_one.once('value', function (snapshot) {
+    let introduce_img = document.getElementById('introduce_img_one');
+    let str = '';
+    let data = snapshot.val();
+    str = `
+        <img src="${data.introduce_one_img1.text}" alt="">
+        <img src="${data.introduce_one_img2.text}" alt="">
+        <img src="${data.introduce_one_img3.text}" alt="">
     
+    `
+    introduce_img.innerHTML = str;
+})
+
+// 文字
+const introduce_one_mainData = db.ref('introduce_one_details');
+introduce_one_mainData.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_mainData');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li>${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+})
+// 介紹
+const introduce_one_introduce = db.ref('introduce_one_introduce');
+introduce_one_introduce.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_introduce');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li>${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+})
+// 設計特點
+const introduce_one_design = db.ref('introduce_one_design');
+introduce_one_design.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_design');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li>${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+})
+
+// 第二組照片
+const introduce_img_two = db.ref('introduce_two');
+introduce_img_two.once('value', function (snapshot) {
+    let introduce_img = document.getElementById('introduce_img_two');
+    let str = '';
+    let data = snapshot.val();
+    str = `
+        <img src="${data.introduce_two_img1.text}" alt="">
+        <img src="${data.introduce_two_img2.text}" alt="">
+        <img src="${data.introduce_two_img3.text}" alt="">
+    
+    `
+    introduce_img.innerHTML = str;
+})
+
+// 第二組文字
+const introduce_two_mainData = db.ref('introduce_two_details');
+introduce_one_mainData.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_two_mainData');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li>${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+})
+
+// 第二組文字
+const introduce_two_introduce = db.ref('details_two_introduce');
+introduce_two_introduce.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_two_introduce');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li>${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+})
+
+// 第二組設計
+const introduce_two_design = db.ref('details_two_design');
+introduce_two_design.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_two_design');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li>${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
 })
--- setting.js
+++ setting.js
@@ -40,3 +40,309 @@ $('.sent2').click(function(){
 
     
 })
+$('.sent3').click(function(){
+
+    let dataText = $(this).data('img1');
+    let inputData = $('#'+dataText+'').val();
+    // console.log(inputData);
+    if(!inputData == ''){
+        db.ref('introduce_one/'+dataText+'').set({text:inputData}).then(function(){
+            $('input[type="text"]').val('');
+        })
+    }else{
+        console.log('空的');
+    }
+
+    
+})
+
+// 產品規格
+$('.sent3_1').click(function(){
+    let dataText = $(this).data('details');
+    let inputData = $('#'+dataText+'').val();
+    // console.log(inputData);
+    if(!inputData == ''){
+        db.ref('introduce_one_details').push({text:inputData}).then(function(){
+            $('input[type="text"]').val('');
+        }).then(function(){
+            alert('新增成功')
+            history.go(0)
+        })
+    }else{
+        console.log('空的');
+    }
+})
+// 文字
+const introduce_one_mainData = db.ref('introduce_one_details');
+introduce_one_mainData.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_mainData');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li class="one_details"  data-key2="${item}">${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+    setTimeout(function(){
+        console.log('ready');
+        
+        $('.one_details').on('click',function(){
+            let datakey = $(this).data('key2');
+            // console.log(datakey);
+            introduce_one_mainData.child(datakey).remove().then(function(){
+                alert('刪除成功')
+                history.go(0)
+            });
+            
+        })
+    },3000)
+    
+})
+
+// var nameRef = firebase.database().ref().child('todos');
+// introduce_one_mainData.child('-N-sBda_BMwvoE7TQErc').remove();
+
+
+
+
+
+// 介紹
+$('.sent3_2').click(function(){
+    let dataText = $(this).data('details');
+    let inputData = $('#'+dataText+'').val();
+    // console.log(inputData);
+    if(!inputData == ''){
+        db.ref('introduce_one_introduce').push({text:inputData}).then(function(){
+            $('input[type="text"]').val('');
+        }).then(function(){
+            alert('新增成功')
+            history.go(0)
+        })
+    }else{
+        console.log('空的');
+    }
+})
+const introduce_one_introduce = db.ref('introduce_one_introduce');
+introduce_one_introduce.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_introduce');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li class="one_details1" data-key2="${item}">${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+    setTimeout(function(){
+        console.log('ready');
+        
+        $('.one_details1').on('click',function(){
+            let datakey = $(this).data('key2');
+            // console.log(datakey);
+            introduce_one_introduce.child(datakey).remove().then(function(){
+                alert('刪除成功')
+                history.go(0)
+            });
+            
+        })
+    },3000)
+
+    
+})
+
+// 設計特點
+$('.sent3_3').click(function(){
+    let dataText = $(this).data('details');
+    let inputData = $('#'+dataText+'').val();
+    if(!inputData == ''){
+        db.ref('introduce_one_design').push({text:inputData}).then(function(){
+            $('input[type="text"]').val('');
+        }).then(function(){
+            alert('新增成功')
+            history.go(0)
+        })
+    }else{
+        console.log('空的');
+    }
+})
+
+const introduce_one_design = db.ref('introduce_one_design');
+introduce_one_design.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_design');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li class="one_details2" data-key2="${item}">${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+    setTimeout(function(){
+        console.log('ready');
+        
+        $('.one_details2').on('click',function(){
+            let datakey = $(this).data('key2');
+            // console.log(datakey);
+            introduce_one_design.child(datakey).remove().then(function(){
+                alert('刪除成功')
+                history.go(0)
+            });
+            
+        })
+    },3000)
+})
+
+
+// 第二組照片
+$('.sent4').click(function(){
+
+    let dataText = $(this).data('img2');
+    let inputData = $('#'+dataText+'').val();
+    // console.log(inputData);
+    if(!inputData == ''){
+        db.ref('introduce_two/'+dataText+'').set({text:inputData}).then(function(){
+            $('input[type="text"]').val('');
+        })
+    }else{
+        console.log('空的');
+    }
+
+    
+})
+//  第二組文字
+$('.sent4_1').click(function(){
+    let dataText = $(this).data('details');
+    let inputData = $('#'+dataText+'').val();
+    // console.log(inputData);
+    if(!inputData == ''){
+        db.ref('introduce_two_details').push({text:inputData}).then(function(){
+            $('input[type="text"]').val('');
+        }).then(function(){
+            alert('新增成功')
+            history.go(0)
+        })
+    }else{
+        console.log('空的');
+    }
+})
+
+//
+const introduce_two_mainData = db.ref('introduce_two_details');
+introduce_two_mainData.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_two_mainData');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li class="one_details3" data-key2="${item}">${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+    setTimeout(function(){
+        console.log('ready');
+        
+        $('.one_details3').on('click',function(){
+            let datakey = $(this).data('key2');
+            // console.log(datakey);
+            introduce_two_mainData.child(datakey).remove().then(function(){
+                alert('刪除成功')
+                history.go(0)
+            });
+            
+        })
+    },3000)
+})
+
+// 第二組介紹
+$('.sent4_2').click(function(){
+    let dataText = $(this).data('details');
+    let inputData = $('#'+dataText+'').val();
+    // console.log(inputData);
+    if(!inputData == ''){
+        db.ref('details_two_introduce').push({text:inputData}).then(function(){
+            $('input[type="text"]').val('');
+        }).then(function(){
+            alert('新增成功')
+            history.go(0)
+        })
+    }else{
+        console.log('空的');
+    }
+})
+
+const introduce_two_introduce = db.ref('details_two_introduce');
+introduce_two_introduce.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_two_introduce_data');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        // console.log(data[item].text);
+        str=`
+        <li class="one_details4" data-key2="${item}">${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+    setTimeout(function(){
+        console.log('ready');
+        
+        $('.one_details4').on('click',function(){
+            let datakey = $(this).data('key2');
+            // console.log(datakey);
+            introduce_two_introduce.child(datakey).remove().then(function(){
+                alert('刪除成功')
+                history.go(0)
+            });
+            
+        })
+    },3000)
+})
+
+// 第二組設計
+$('.sent4_3').click(function(){
+    let dataText = $(this).data('details');
+    let inputData = $('#'+dataText+'').val();
+    // console.log(inputData);
+    if(!inputData == ''){
+        db.ref('details_two_design').push({text:inputData}).then(function(){
+            $('input[type="text"]').val('');
+        }).then(function(){
+            alert('新增成功')
+            history.go(0)
+        })
+    }else{
+        console.log('空的');
+    }
+})
+
+// 
+const introduce_two_design = db.ref('details_two_design');
+introduce_two_design.once('value', function (snapshot) {
+    let introduce_mainData = document.getElementById('details_two_design_data');
+    let str = '';
+    let data = snapshot.val();
+    for (item in data) {
+        
+        str=`
+        <li class="one_details5" data-key2="${item}">${data[item].text}</li>
+        `
+        introduce_mainData.innerHTML +=str;
+    }
+    setTimeout(function(){
+        console.log('ready');
+        
+        $('.one_details5').on('click',function(){
+            let datakey = $(this).data('key2');
+            // console.log(datakey);
+            introduce_two_design.child(datakey).remove().then(function(){
+                alert('刪除成功')
+                history.go(0)
+            });
+            
+        })
+    },3000)
+})
